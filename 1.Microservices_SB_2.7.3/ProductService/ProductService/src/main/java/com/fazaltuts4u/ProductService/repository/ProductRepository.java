85b34110bb6be92b352f56a228f23aceaf71644b
Software Developer <kharoon187@gmail.com>
2023-05-26 11:22:30 +0500

    Creating Product service/module repository

--- 1.Microservices_SB_2.7.3/ProductService/ProductService/src/main/java/com/fazaltuts4u/ProductService/repository/ProductRepository.java
+++ 1.Microservices_SB_2.7.3/ProductService/ProductService/src/main/java/com/fazaltuts4u/ProductService/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.fazaltuts4u.ProductService.repository;
 
 import com.fazaltuts4u.ProductService.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
