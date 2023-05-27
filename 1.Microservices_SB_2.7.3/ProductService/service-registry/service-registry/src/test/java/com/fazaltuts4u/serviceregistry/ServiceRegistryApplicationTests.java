7a0ff7316702bcdfc732f1cbbcc9e91fedce7899
Fazal Haroon <36573782+Fazal-haroon@users.noreply.github.com>
2023-05-27 11:17:20 +0500

    added service-registry

---
+++ 1.Microservices_SB_2.7.3/ProductService/service-registry/service-registry/src/test/java/com/fazaltuts4u/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fazaltuts4u.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
