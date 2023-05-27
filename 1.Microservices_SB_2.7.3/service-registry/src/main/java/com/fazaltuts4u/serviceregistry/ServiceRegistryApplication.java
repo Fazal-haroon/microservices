cfaf25f584e76e6689d5e54177aaf27592f09221
Software Developer <kharoon187@gmail.com>
2023-05-27 11:59:04 +0500

    Service Registry configuration

--- 1.Microservices_SB_2.7.3/service-registry/src/main/java/com/fazaltuts4u/serviceregistry/ServiceRegistryApplication.java
+++ 1.Microservices_SB_2.7.3/service-registry/src/main/java/com/fazaltuts4u/serviceregistry/ServiceRegistryApplication.java
@@ -2,7 +2,9 @@ package com.fazaltuts4u.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegistryApplication {
 
