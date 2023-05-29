93641dd3ca00ad0c56035fc3a212299f169264e6
Software Developer <kharoon187@gmail.com>
2023-05-29 23:59:27 +0500

    Implementing ErrorDecoder to handle Exceptions part 3

---
+++ 1.Microservices_SB_2.7.3/OrderService/src/main/java/com/fazaltuts4u/OrderService/exception/CustomException.java
@@ -0,0 +1,12 @@
+package com.fazaltuts4u.OrderService.exception;
+
+public class CustomException extends RuntimeException{
+    private String errorCode;
+    private int status;
+
+    public CustomException(String message, String errorCode, int status) {
+        super(message);
+        this.errorCode = errorCode;
+        this.status = status;
+    }
+}
