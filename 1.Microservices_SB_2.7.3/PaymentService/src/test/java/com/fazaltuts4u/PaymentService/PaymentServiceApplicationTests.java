2958fa7429ab79e26411a9fc073186c5f225ab4b
Fazal Haroon <36573782+Fazal-haroon@users.noreply.github.com>
2023-05-30 18:30:47 +0500

    Added Payment Service

---
+++ 1.Microservices_SB_2.7.3/PaymentService/src/test/java/com/fazaltuts4u/PaymentService/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fazaltuts4u.PaymentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
