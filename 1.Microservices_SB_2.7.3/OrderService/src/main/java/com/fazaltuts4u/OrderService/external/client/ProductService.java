eada86f991573f28ea0fed26bcb46238a304b4eb
Software Developer <kharoon187@gmail.com>
2023-06-01 23:32:15 +0500

    Implementing Circuit Breaker in Order Service

--- 1.Microservices_SB_2.7.3/OrderService/src/main/java/com/fazaltuts4u/OrderService/external/client/ProductService.java
+++ 1.Microservices_SB_2.7.3/OrderService/src/main/java/com/fazaltuts4u/OrderService/external/client/ProductService.java
@@ -1,13 +1,20 @@
 package com.fazaltuts4u.OrderService.external.client;
 
+import com.fazaltuts4u.OrderService.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
     @PutMapping("/reduceQuantity/{id}")
     ResponseEntity<Void> reduceQuantity(@PathVariable("id") long productId, @RequestParam long quantity);
+
+    default void fallback(Exception e){
+        throw new CustomException("Product Service is not Available", "UNAVAILABLE", 500);
     }
+}
