eb7ead44ef1389f463f422cb8266ec67ec4b67fe
Software Developer <kharoon187@gmail.com>
2023-06-02 23:40:00 +0500

    Adding Security in API Gateway

---
+++ 1.Microservices_SB_2.7.3/CloudGateway/src/main/java/com/fazaltuts4u/CloudGateway/security/OktaOAuth2WebSecurity.java
@@ -0,0 +1,25 @@
+package com.fazaltuts4u.CloudGateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class OktaOAuth2WebSecurity {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity serverHttpSecurity){
+        serverHttpSecurity
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2Login()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return serverHttpSecurity.build();
+    }
+}
