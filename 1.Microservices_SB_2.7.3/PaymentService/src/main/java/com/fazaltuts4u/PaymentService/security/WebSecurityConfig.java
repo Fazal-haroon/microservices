f06dc9757d20b5d19b89232fa4b59ab33ce93d0e
Software Developer <kharoon187@gmail.com>
2023-06-04 12:25:06 +0500

    Adding Security in Payment Service with Okta

---
+++ 1.Microservices_SB_2.7.3/PaymentService/src/main/java/com/fazaltuts4u/PaymentService/security/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.fazaltuts4u.PaymentService.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers("/payment/**")
+                                .hasAuthority("SCOPE_internal")
+                                .anyRequest()
+                                .authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        return httpSecurity.build();
+    }
+}
