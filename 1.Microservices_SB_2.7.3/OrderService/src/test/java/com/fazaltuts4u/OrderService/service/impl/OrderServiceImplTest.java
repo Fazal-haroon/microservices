610c2273e57d3effe1a560205ff5c4a76236e296
Software Developer <kharoon187@gmail.com>
2023-06-05 23:37:40 +0500

    Unit Testing: Create a Test Method with Mocking

--- 1.Microservices_SB_2.7.3/OrderService/src/test/java/com/fazaltuts4u/OrderService/service/impl/OrderServiceImplTest.java
+++ 1.Microservices_SB_2.7.3/OrderService/src/test/java/com/fazaltuts4u/OrderService/service/impl/OrderServiceImplTest.java
@@ -1,15 +1,27 @@
 package com.fazaltuts4u.OrderService.service.impl;
 
+import com.fazaltuts4u.OrderService.entity.Order;
 import com.fazaltuts4u.OrderService.external.client.PaymentService;
 import com.fazaltuts4u.OrderService.external.client.ProductService;
+import com.fazaltuts4u.OrderService.external.response.PaymentResponse;
+import com.fazaltuts4u.OrderService.external.response.ProductResponse;
+import com.fazaltuts4u.OrderService.model.PaymentMode;
 import com.fazaltuts4u.OrderService.repository.OrderRepository;
 import com.fazaltuts4u.OrderService.service.OrderService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Instant;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class OrderServiceImplTest {
@@ -33,10 +45,55 @@ public class OrderServiceImplTest {
     @Test
     void test_When_Order_Success(){
         //Mocking
+        Order order = getMockOrder();
+        when(orderRepository.findById(anyLong()))
+                .thenReturn(Optional.of(order));
+
+        when(restTemplate.getForObject(
+                "http://PRODUCT-SERVICE/product/" + order.getProductId(),
+                ProductResponse.class)
+        ).thenReturn(getMockProductResponse());
+
+        when(restTemplate.getForObject(
+                "http://PAYMENT-SERVICE/payment/order/" + order.getId(),
+                PaymentResponse.class)
+        ).thenReturn(getMockPaymentResponse());
         //Actual
+        orderService.getOrderDetails(9);
         //Verification
         //Assert
 
+    }
+
+    private PaymentResponse getMockPaymentResponse() {
+        return PaymentResponse.builder()
+                .orderId(9)
+                .paymentDate(Instant.now())
+                .amount(100)
+                .paymentMode(PaymentMode.CASH)
+                .status("ACCEPTED")
+                .paymentId(1)
+                .build();
+    }
+
+    private ProductResponse getMockProductResponse() {
+        return ProductResponse.builder()
+                .productId(2)
+                .productName("iPhone")
+                .price(100)
+                .quantity(200)
+                .build();
+    }
+
+    private Order getMockOrder() {
+        return Order.builder()
+                .orderStatus("PLACED")
+                .orderDate(Instant.now())
+                .id(9)
+                .amount(100)
+                .quantity(200)
+                .productId(2)
+                .build();
     }
 
 }
