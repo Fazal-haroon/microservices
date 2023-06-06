82d16308eba964f791a67c577ca8506c8507e3d8
Software Developer <kharoon187@gmail.com>
2023-06-06 23:32:17 +0500

    Unit Testing: Creating OrderControllerTest Class

---
+++ 1.Microservices_SB_2.7.3/OrderService/src/test/java/com/fazaltuts4u/OrderService/controller/OrderControllerTest.java
@@ -0,0 +1,16 @@
+package com.fazaltuts4u.OrderService.controller;
+
+
+import com.fazaltuts4u.OrderService.OrderServiceConfig;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest({"server.port=0"})
+@EnableConfigurationProperties
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = {OrderServiceConfig.class})
+public class OrderControllerTest {
+
+}
