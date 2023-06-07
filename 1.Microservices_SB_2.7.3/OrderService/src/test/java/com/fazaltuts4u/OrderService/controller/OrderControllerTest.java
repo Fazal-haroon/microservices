752abd179006864a6e337719e9e8678dc32932cd
Software Developer <kharoon187@gmail.com>
2023-06-07 22:56:48 +0500

    Unit Testing: Implementing Setup Method for PlaceOrder

--- 1.Microservices_SB_2.7.3/OrderService/src/test/java/com/fazaltuts4u/OrderService/controller/OrderControllerTest.java
+++ 1.Microservices_SB_2.7.3/OrderService/src/test/java/com/fazaltuts4u/OrderService/controller/OrderControllerTest.java
@@ -7,15 +7,28 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fazaltuts4u.OrderService.OrderServiceConfig;
 import com.fazaltuts4u.OrderService.repository.OrderRepository;
 import com.fazaltuts4u.OrderService.service.OrderService;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static java.nio.charset.Charset.*;
+import static org.springframework.util.StreamUtils.*;
 
 @SpringBootTest({"server.port=0"})
 @EnableConfigurationProperties
@@ -46,4 +59,62 @@ public class OrderControllerTest {
             .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
+    @BeforeEach
+    void setup() throws IOException {
+        getProductDetailsResponse();
+        doPayment();
+        getPaymentDetails();
+        reduceQuantity();
+    }
+
+    private void doPayment() {
+        wireMockExtension.stubFor(post(urlEqualTo("/payment"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)));
+    }
+
+    private void getPaymentDetails() throws IOException {
+        wireMockExtension.stubFor(get(urlMatching("/payment/.*"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(
+                                copyToString(
+                                        OrderControllerTest.class
+                                                .getClassLoader()
+                                                .getResourceAsStream("mock/GetPayment.json"),
+                                        defaultCharset()
+                                )
+                        )));
+    }
+
+    private void reduceQuantity() {
+        wireMockExtension.stubFor(put(urlMatching("/product/reduceQuantity/.*"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)));
+    }
+
+    private void getProductDetailsResponse() throws IOException {
+        // GET /product/1
+        wireMockExtension.stubFor(get("/product/1")
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(copyToString(
+                                OrderControllerTest.class
+                                        .getClassLoader()
+                                        .getResourceAsStream("mock/GetProduct.json"),
+                                defaultCharset()
+                        )))
+                );
+    }
+
+    @Test
+    public void test_WhenPlaceOrder_DoPayment_Success() {
+        //First Place Order
+        //Get Order by OrderId from DB and check
+        //Check Output
+    }
 }
