1949f31b69dbda674b5ffff0c2a5fb1237ecf4a7
Software Developer <kharoon187@gmail.com>
2023-06-07 23:55:06 +0500

    Unit Testing: PlaceOrder Test GetOrder Success Scenario Part 2

--- 1.Microservices_SB_2.7.3/OrderService/src/test/java/com/fazaltuts4u/OrderService/controller/OrderControllerTest.java
+++ 1.Microservices_SB_2.7.3/OrderService/src/test/java/com/fazaltuts4u/OrderService/controller/OrderControllerTest.java
@@ -1,7 +1,6 @@
 package com.fazaltuts4u.OrderService.controller;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -12,10 +11,8 @@ import com.fazaltuts4u.OrderService.model.OrderResponse;
 import com.fazaltuts4u.OrderService.model.PaymentMode;
 import com.fazaltuts4u.OrderService.repository.OrderRepository;
 import com.fazaltuts4u.OrderService.service.OrderService;
-import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -26,23 +23,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.util.StreamUtils;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.Optional;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static java.nio.charset.Charset.*;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
-import static org.springframework.util.StreamUtils.*;
+import static java.nio.charset.Charset.defaultCharset;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.util.StreamUtils.copyToString;
 
 @SpringBootTest({"server.port=0"})
 @EnableConfigurationProperties
@@ -186,6 +181,8 @@ public class OrderControllerTest {
         String actualResponse = mvcResult.getResponse().getContentAsString();
         Order order = orderRepository.findById(1l).get();
         String expectedResponse = getOrderResponse(order);
+
+        assertEquals(expectedResponse,actualResponse);
     }
 
     private String getOrderResponse(Order order) throws IOException {
@@ -198,5 +195,25 @@ public class OrderControllerTest {
                         defaultCharset()
                 ), OrderResponse.PaymentDetails.class
         );
+
+        OrderResponse.ProductDetails productDetails
+                = objectMapper.readValue(
+                copyToString(
+                        OrderControllerTest.class.getClassLoader()
+                                .getResourceAsStream("mock/GetProduct.json"),
+                        defaultCharset()
+                ), OrderResponse.ProductDetails.class
+        );
+
+        OrderResponse orderResponse
+                = OrderResponse.builder()
+                .paymentDetails(paymentDetails)
+                .productDetails(productDetails)
+                .orderStatus(order.getOrderStatus())
+                .orderDate(order.getOrderDate())
+                .amount(order.getAmount())
+                .orderId(order.getId())
+                .build();
+        return objectMapper.writeValueAsString(orderResponse);
     }
 }
